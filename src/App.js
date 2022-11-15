c21e8af2de1bd73384d9b7d83db0adfee3f3a99e
Mateo P. Cetti <mateocetti2000@gmail.com>
2022-11-15 11:48:06 -0300

    Add some css

--- src/App.js
+++ src/App.js
@@ -6,25 +6,35 @@ function App() {
   const [queryName, setQueryName] = useState("")
   const [pokemon, setPokemon] = useState(null)
   return (
-    <div className="App">
-      <h1>Pokedex</h1>
-      <form onSubmit={async (event) => {
-        event.preventDefault();
-        const sape= await fetch(`http://0.0.0.0:3030/?name=${queryName}`);
-        const new_pokemon = await sape.json();
-        console.log(new_pokemon)
-        setPokemon(new_pokemon)}}>
-      <input
-          type="text" 
-          value={queryName}
-          placeholder="Charizard"
-          onChange={(e) => setQueryName(e.target.value)}
-        />
-      <button type="submit">Get pokemon types</button>
-      </form>
-    {pokemon != null && (Object.keys(pokemon).length > 1 ? 
-    <div>{pokemon.name}, {pokemon.types[0]}, {pokemon.types[1]}</div>
-  : <div>Error: {pokemon.error}</div>)}
+    <div className="Container text-white">
+      <div className='row'>
+        <div className='col-12 text-center'>
+          <h1>Pokedex</h1>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-12 text-center'>
+          <form onSubmit={async (event) => {
+            event.preventDefault();
+            const sape = await fetch(`http://0.0.0.0:3030/?name=${queryName}`);
+            const new_pokemon = await sape.json();
+            console.log(new_pokemon)
+            setPokemon(new_pokemon)
+          }}>
+            <input
+              type="text"
+              value={queryName}
+              placeholder="Charizard"
+              onChange={(e) => setQueryName(e.target.value)}
+            />
+            <button type="submit" className="btn btn-primary">Get pokemon types</button>
+          </form>
+
+        </div>
+      </div>
+      {pokemon != null && (Object.keys(pokemon).length > 1 ?
+        <div className='text-white'>{pokemon.name}, {pokemon.types[0]}, {pokemon.types[1]}</div>
+        : <div className='text-white'>Error: {pokemon.error}</div>)}
     </div>
   );
 }
