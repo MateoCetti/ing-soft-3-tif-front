6a034688188303c6835e3959fff7db44b620ee09
Mateo P. Cetti <mateocetti2000@gmail.com>
2022-11-16 19:40:57 -0300

    Add a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
   const linkElement = screen.getByText(/pokemon/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders button', ()=>{
+  const res = render(<App />);
+  const linkElement = res.getByText("Get pokemon");
+  expect(linkElement).toBeInTheDocument();
+})
