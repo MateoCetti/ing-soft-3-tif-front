7b557ea41a45aaa794387636c3cc1ab03a750ab2
Mateo P. Cetti <mateocetti2000@gmail.com>
2022-11-15 23:40:52 -0300

    Edit test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Get pokemon types/i);
+  const linkElement = screen.getByText(/pokemon/i);
   expect(linkElement).toBeInTheDocument();
 });
