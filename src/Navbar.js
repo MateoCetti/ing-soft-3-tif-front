513db6b7e74e10d54122dea88b24b6ed0607059f
Mateo P. Cetti <mateocetti2000@gmail.com>
2022-12-02 16:47:49 -0300

    Revert last change

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <>
-      <nav id="my-navbar" className="navbar w-100 navbar-dark bg-warning ">
+      <nav id="my-navbar" className="navbar w-100 navbar-dark bg-danger ">
         <div className="container-fluid ">
           <a className="navbar-brand" href={window.location.href}>
             Pokedex
